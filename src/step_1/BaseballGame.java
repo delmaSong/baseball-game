faa47478f0c2e2fdc9707b01cbff0a56af49e6c7
dayeong <sdy2856@gmail.com>
2019-11-29 23:15:53 +0900

    1단계 완료

---
+++ src/step_1/BaseballGame.java
@@ -0,0 +1,75 @@
+package step_1;
+
+import java.util.Random;
+
+public class BaseballGame {
+	
+	Random ran = new Random();
+	int strikeCnt = 0;
+	int ballCnt = 0;
+	int outCnt = 0;
+	int antaCnt = 0;
+	
+	public static void main(String[] args) {
+		BaseballGame bg = new BaseballGame();
+		bg.playGame();
+	}
+
+
+	public void drawRandom() {
+		int randomResult = (ran.nextInt(3)+0);
+		if(randomResult == 0) {
+			System.out.print("\n스트라이크! ");
+			strikeCnt += 1;
+		}else if(randomResult == 1) {
+			System.out.print("\r볼! ");
+			ballCnt += 1;
+		}else if(randomResult == 2) {
+			System.out.print("\r아웃! ");
+			outCnt += 1 ;
+			comeNextHitter();
+		}else {
+			System.out.print("\r안타! ");
+			antaCnt += 1;
+			comeNextHitter();
+		}
+	}
+	
+	public void playGame() {
+		System.out.println("신나는 야구 게임! \n첫 번째 타자가 타석에 입장했습니다.");
+		while(outCnt != 3) {
+			drawRandom();
+			cntPoint();
+		}
+		if (outCnt == 3) {
+			finishGame();
+		}
+	}
+	
+	public void finishGame() {
+		System.out.println("\n최종 안타수: " + antaCnt);
+		System.out.println("GAME OVER");
+	}
+	
+	public void cntPoint() {
+		if ( strikeCnt == 3 ) {
+			strikeCnt = 0;
+			outCnt += 1;
+		}
+		if (ballCnt == 4) {
+			ballCnt = 0;
+			antaCnt += 1;
+		}
+//		if (outCnt == 1 || antaCnt == 1) {
+//			comeNextHitter();
+//		}
+		System.out.println("\n"+ strikeCnt+"S "+ballCnt+"B " + outCnt+"O ");
+	}
+	
+	public void comeNextHitter() {
+		System.out.print("다음 타자가 타석에 입장했습니다.");
+		strikeCnt = 0;
+		ballCnt = 0;
+	}
+	
+}
