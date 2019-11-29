2a797abd5a11951b0c64be5cb537bcce89e005bf
dayeong <sdy2856@gmail.com>
2019-11-29 23:23:09 +0900

    리팩토링

--- src/baseball_game/BaseballGame.java
+++ src/baseball_game/BaseballGame.java
@@ -1,10 +1,11 @@
-package step_1;
+package baseball_game;
 
 import java.util.Random;
 
 public class BaseballGame {
-	
 	Random ran = new Random();
+	int randomResult = (ran.nextInt(3)+0);
+
 	int strikeCnt = 0;
 	int ballCnt = 0;
 	int outCnt = 0;
@@ -15,40 +16,31 @@ public class BaseballGame {
 		bg.playGame();
 	}
 
-
-	public void drawRandom() {
-		int randomResult = (ran.nextInt(3)+0);
-		if(randomResult == 0) {
-			System.out.print("\n스트라이크! ");
-			strikeCnt += 1;
-		}else if(randomResult == 1) {
-			System.out.print("\r볼! ");
-			ballCnt += 1;
-		}else if(randomResult == 2) {
-			System.out.print("\r아웃! ");
-			outCnt += 1 ;
-			comeNextHitter();
-		}else {
-			System.out.print("\r안타! ");
-			antaCnt += 1;
-			comeNextHitter();
-		}
-	}
-	
 	public void playGame() {
 		System.out.println("신나는 야구 게임! \n첫 번째 타자가 타석에 입장했습니다.");
 		while(outCnt != 3) {
 			drawRandom();
 			cntPoint();
 		}
-		if (outCnt == 3) {
 			finishGame();
-		}
 	}
-	
-	public void finishGame() {
-		System.out.println("\n최종 안타수: " + antaCnt);
-		System.out.println("GAME OVER");
+
+	public void drawRandom() {
+		if(randomResult == 0) {
+			System.out.print("\n스트라이크! ");
+			strikeCnt += 1;
+		}else if(randomResult == 1) {
+			System.out.print("\n볼! ");
+			ballCnt += 1;
+		}else if(randomResult == 2) {
+			System.out.print("\n아웃! ");
+			outCnt += 1 ;
+			comeNextHitter();
+		}else {
+			System.out.print("\n안타! ");
+			antaCnt += 1;
+			comeNextHitter();
+		}
 	}
 	
 	public void cntPoint() {
@@ -60,9 +52,6 @@ public class BaseballGame {
 			ballCnt = 0;
 			antaCnt += 1;
 		}
-//		if (outCnt == 1 || antaCnt == 1) {
-//			comeNextHitter();
-//		}
 		System.out.println("\n"+ strikeCnt+"S "+ballCnt+"B " + outCnt+"O ");
 	}
 	
@@ -70,6 +59,11 @@ public class BaseballGame {
 		System.out.print("다음 타자가 타석에 입장했습니다.");
 		strikeCnt = 0;
 		ballCnt = 0;
+	}
+	
+	public void finishGame() {
+		System.out.println("\n최종 안타수: " + antaCnt);
+		System.out.println("GAME OVER");
 	}
 	
 }
